c488180a4ddc424072a8ad172a88694114cb1fb6
pedrodaniluz <pdaniluz.dev@gmail.com>
2026-08-26 21:21:25 -0300

    feat: initial commit

---
+++ src/test/java/com/codaxistech/argus/ArgusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codaxistech.argus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
